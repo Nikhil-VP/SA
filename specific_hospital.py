4bd7615442d0c1b5f20a6e467f7c7b341e63fb60
Nikhil-VP <nikhilvp888@gmail.com>
2025-01-05 16:32:21 +0530

    Bugs fix

--- specific_hospital.py
+++ specific_hospital.py
@@ -1,12 +1,16 @@
 import streamlit as st
 from utils import display_map
-
+from nearest_hospital import nearest_hospital_page
 def specific_hospital_page():
     st.title("Specific Hospital")
     
     # Input fields for hospital and doctor search
     st.text_input("Type hospital name", placeholder="Search for a hospital")
     st.text_input("Type doctor name", placeholder="Search for a doctor")
+
+    connect =st.button("Connect")
+    if connect:
+        nearest_hospital_page()
     
     # Patient Details
     st.subheader("Patient Details")
